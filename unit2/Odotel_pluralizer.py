55a6f01f3c4603fb838b382fefd30601840b36e1
Oscary Dotel <oscarydotel@uagateway.org>
2016-10-13 19:02:54 +0000

    Finished unit 2 lab 2

--- unit2/Odotel_pluralizer.py
+++ unit2/Odotel_pluralizer.py
@@ -1,19 +1,22 @@
 nu= input("Enter a Number ")
 n = input("Enter a noun ")
-if n[-2:]=="fe":
+if nu=="1":
+ print(nu+" "+ n)
+elif n[-2:]=="ay":
+    print(nu +" "+ n+ "s")
+elif n[-2:]== "oy":
+    print(nu +" "+ n+ "s")
+elif n[-2:]=="ey":
+    print(nu +" "+ n+ "s")
+elif n[-2:]=="uy":
+    print(nu +" "+ n+ "s")
+elif n[-2:]=="fe":
     print(nu +" "+ n[0:-2]+"ves")
-if n[-1]=="y":
+elif n[-1:]=="y":
     print(nu +" "+ n[0:-1]+ "ies")
-if n[-2]=="sh":
-    print(nu +" "+ n[0:-2]+ "es")
-if n[-2]== "us":
+elif n[-2:]=="sh":
+    print(nu +" "+ n+ "es")
+elif n[-2:]== "us":
     print(nu +" "+ n[0:-2]+ "i")
-if n[-2]=="ay":
-    print(nu +" "+ n+ "s")
-if n[-2]== "oy":
-    print(nu +" "+ n+ "s")
-if n[-2]=="ey":
-    print(nu +" "+ n+ "s")
-if n[-2]=="uy":
-    print(nu +" "+ n+ "s")
-else: print(nu +" "+n + "s")
+else: 
+    print(nu +" "+n + "s")
