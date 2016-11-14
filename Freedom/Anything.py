63c8c598d57d6c89aa8d0e262f46af28108e76d1
Oscary Dotel <oscarydotel@uagateway.org>
2016-11-14 17:16:08 +0000

    Trying to complete rps

---
+++ Freedom/Anything.py
@@ -0,0 +1,8 @@
+# Want this to do something but it has to be complainted.mmmm wished I had ideas... Hmmmm A.I ? making an A.I would be great but what would this A.I do ? Gonna try to think about that this week, let's see what happens
+a = input("A")
+def add_one(a):
+    print("in function: a is {}".format(a))
+
+a = input("anthing")
+
+add_one(a)
