0d3cab7dce80fd22b66b99dfb85f5fc1213a5097
Oscary Dotel <oscarydotel@uagateway.org>
2016-12-14 17:16:43 +0000

    Tried to finish up Phase two.. but I'm having troubles.. Idk how to assign a varieble in oe function to another as a argument

--- Unit4/ODotel_Craps.py
+++ Unit4/ODotel_Craps.py
@@ -12,8 +12,8 @@ def DICEroll():
     dice_uno = random.randint(1,6)
     dice_dos = random.randint(1,6)
     dice_add = dice_uno + dice_dos
-    Print("Dice Uno {} and Dice Dos {} \n".format(dice_uno,dice_dos))
-    Print("total is {}".format(dice_add))
+    print("Dice Uno {} and Dice Dos {} \n".format(dice_uno,dice_dos))
+    print("total is {}".format(dice_add))
     return dice_add
     
 def PHASE_UNO(player_Dinero):
@@ -22,10 +22,21 @@ def PHASE_UNO(player_Dinero):
      if Dinero_amount > player_cash or Dinero_amount <= 0:
          Dinero_amount = int(("You betted wrong"))
      return Dinero_amount
-     
-def PHASE_DOS(dice_uno,dice_add,dice_add):
-     print("Your Roll are:\nDice Uno {} !\nDice Dos {}!\nTotal is {} !" .format(dice_uno,dice_dos,dice_add))
-    
+ 
+def PHASE_DOS(dice_uno,dice_add,dice_dos,):
+    print("Your Roll are:\nDice Uno {} !\nDice Dos {}!\nTotal is {} !" .format(dice_uno,dice_dos,dice_add))
+    if dice_add == 2:
+        return "You lost"
+    elif dice_add == 3 :
+        return "You lost"
+    elif dice_add == 12 :
+        return "You lost"
+    elif dice_add == 7 :
+        return "You Won"
+    elif dice_add == 11 :
+        return "You Won"
+    else:
+        return dice_add
 
 DICEroll()
 PHASE_DOS()
