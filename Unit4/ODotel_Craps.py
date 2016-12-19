373e201e75d7ec8d3cbe01e9eb506a2e511f5487
Oscary Dotel <oscarydotel@uagateway.org>
2016-12-19 17:15:53 +0000

    Worked on my game functions that puts all my other functions together

--- Unit4/ODotel_Craps.py
+++ Unit4/ODotel_Craps.py
@@ -12,12 +12,12 @@ def DICEroll():
     dice_uno = random.randint(1,6)
     dice_dos = random.randint(1,6)
     dice_add = dice_uno + dice_dos
-    print("Your Roll are:\nDice Uno {} !\nDice Dos {}!\nTotal is {} !" .format(dice_uno,dice_dos,dice_add))
+    print("Your Roll are:\nDice Uno {}!\nDice Dos {}!\nTotal is {}!" .format(dice_uno,dice_dos,dice_add))
     return dice_add
     
 def PHASE_UNO():
      player_cash = Player_Dinero()
-     Dinero_amount = int(input("Enter amount of bet!\n--RULES FOR A CORRECT BET--\n(1)Bet can not be negative \n(2)Must not be more of what you bet \n(3)Must only be whole numbers\n(4)Must obvious ONLY NUMBERS\n\n\nENTER BET HERE:"))
+     Dinero_amount = int(input("Enter amount of bet!\n--RULES FOR A CORRECT BET--\n(1)Bet can not be negative \n(2)Must not be more of what you have \n(3)Must only be whole numbers\n(4)Must obviously ONLY NUMBERS\n\n\nENTER BET HERE:"))
      while Dinero_amount > player_cash or Dinero_amount <= 0:
          print("You bet a wrong number!\nTry again!")
          Dinero_amount = int(input("Enter a correct amount of money for a bet\nPlease READ what is allowed for a CORRECT BET ABOVE!!!\nPlease try again: "))
@@ -47,13 +47,17 @@ def PHASE_TRES():
          dice_dos_Ph3 = random.randint(1,6)
          dice_add_Ph3 = dice_uno_Ph3 + dice_dos_Ph3
      if dice_add_Ph3 == 7:
-         return "Lost"
+         print "Lost"
      elif dice_add_Ph3 == point_num:
-        return "Won"
+         print "Won"
 
-print("Casino Royal")
-Player_name = input("What is your name Sir/Ma?\nMy name is ----- .")
-print("Welcome " + Player_name + " to craps!!")
-Player_Dinero()
-PHASE_UNO()
-PHASE_DOS()
+def game():
+    print("Casino Royale")
+    player_cash = Player_Dinero()
+    
+    while player_cash > 0:
+        PHASE_UNO()
+        PHASE_DOS()
+        # PHASE_TRES()
+        
+game()
