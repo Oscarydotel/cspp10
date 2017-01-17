70d3bb1191b35135b7ef1f1a5f07b524c0373bca
Oscary Dotel <oscarydotel@uagateway.org>
2017-01-17 17:16:07 +0000

    worked on lab 2

--- Freedom/Anything.py
+++ Freedom/Anything.py
@@ -1,9 +1,5 @@
-# Want this to do something but it has to be complainted.mmmm wished I had ideas... Hmmmm A.I ? making an A.I would be great but what would this A.I do ? Gonna try to think about that this week, let's see what happens
-a = input("A")
-def add_one(a):
-    print("in function: a is {}".format(a))
-
-a = input("anthing")
-
-add_one(a)
-#
+number_list = []
+user_input = input("enter num")
+if user_input > 0:
+    number_list.append(user_input)
+    print(number_list)
--- Unit5/mp_list2.py
+++ Unit5/mp_list2.py
@@ -1,3 +1,19 @@
+#Worked With Cecilia  
 number_list = []
 user_input = input("Enter\nNumber\nSum\nClear\nPrint\nLenght\nor Exit\nPlease:  ")
 print(user_input)
+while user_input != "Exit" or user_input != "exit":
+    if user_input == "Clear":
+        number_list = []
+        print(number_list)
+    elif user_input == "Print":
+        print(number_list)
+    elif user_input == "Length":
+        print(len(number_list))
+    elif user_input == "Sum":
+        print(sum(user_input))
+    else:
+        user_input = int(user_input)
+        number_list.append(user_input)
+
+
