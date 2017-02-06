afcf1172eaca014a39c5df212b8f40eede066b20
Oscary Dotel <oscarydotel@uagateway.org>
2017-02-06 17:16:39 +0000

    Worked on scramble.. wit the help of classmates i was able to finish the scramble_word function

--- Unit5/ODotel_scramble.py
+++ Unit5/ODotel_scramble.py
@@ -3,11 +3,26 @@
 # Split each and every word in WORD
 #Then put them into a list
 #seperate each word into a list
-# 
+# number_list.append(user_input)
 import random
 
-def scramble_word():
-    WORD = str(input("Enter a Sent: "))
-    a = WORD.split(" ")
-    print(a)
-scramble_word()
+word = str(input("Enter A word "))
+words = str(input("Enter a WORDS"))
+slic = words.split(" ")
+print(slic)
+print(word)
+def scramble_word(word):
+    x1 = word[0]
+    x2 = word[-1]
+    inside = word[1:-1]
+    lst = list(inside)
+    random.shuffle(lst)
+    lst.append(x2)
+    lst.insert(0,x1)
+    word = "".join(lst)
+    print(word)
+    
+    
+    
+    
+scramble_word(word)
